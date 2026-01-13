63429bde6a67d5ac0cd14487c783ec16ff9755cc
Ubuntu <ubuntu@ip-172-31-2-5.ap-south-1.compute.internal>
2026-01-13 20:57:27 +0000

    Update outputs.tf for alb-rule

--- dev/application/employee-app/alb-rule/outputs.tf
+++ dev/application/employee-app/alb-rule/outputs.tf
@@ -1,7 +1,9 @@
 output "listener_rule_arn" {
   value = aws_lb_listener_rule.employee_api.arn
 }
+
 output "alb_sg_id" {
-  value = aws_security_group.employee_alb_sg.id
+  value       = data.terraform_remote_state.alb.outputs.alb_sg_id
   description = "Security group ID of the ALB"
 }
+
